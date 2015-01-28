27a0bab631a08fe35305f5624b07d4f969cfd47a
lber19535 <lber19535@126.com>
2015-01-28 23:50:48 +0800

    [Bill Lv] 第一章导言部分

--- src/com/bill/chapter/one/BasicModel.java
+++ src/com/bill/chapter/one/BasicModel.java
@@ -1,5 +1,30 @@
 package com.bill.chapter.one;
 
+/**
+ * 这里包含了第一章的导言（介绍了一个求最大公约数的递归方法）
+ * 和第一节基础编程模型
+ * 
+ * 
+ * @author Bill Lv
+ *
+ */
 public class BasicModel {
-
+	
+	public static void main(String[] args) {
+		
+		System.out.println(gdc(24, 32));	//8
+	}
+	
+	/*
+	 * 最大公约数，欧几里得算法
+	 */
+	public static int gdc(int p, int q){
+		if (q == 0) {
+			return p;
+		}else {
+			int r = p % q;
+			return gdc(q, r);
+		}
+		
+	}
 }
