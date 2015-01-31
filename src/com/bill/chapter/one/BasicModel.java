e9939b6fe04ca359729b478fd420a163df6a2dbb
lber19535 <lber19535@126.com>
2015-01-31 17:59:23 +0800

    [Bill Lv] 添加所需lib文件，由于Std标准库没有包名无法import，所以将其源码放于项目下

--- src/com/bill/chapter/one/BasicModel.java
+++ src/com/bill/chapter/one/BasicModel.java
@@ -1,29 +1,40 @@
 package com.bill.chapter.one;
 
+import com.bill.std.utils.StdDraw;
+
 /**
- * 这里包含了第一章的导言（介绍了一个求最大公约数的递归方法）
- * 和第一节基础编程模型
+ * 这里包含了第一章的导言（介绍了一个求最大公约数的递归方法） 和第一节基础编程模型
  * 
  * 
  * @author Bill Lv
  *
  */
 public class BasicModel {
-	
+
 	public static void main(String[] args) {
-		
+
+		int N = 1000;
+		StdDraw.setXscale(0, N);
+		StdDraw.setYscale(0, N * N);
+		StdDraw.setPenRadius(0.01);
+		for (int i = 0; i < N; i++) {
+			StdDraw.point(i, i);
+			StdDraw.point(i, i * i);
+			StdDraw.point(i, i * Math.log(i));
+		}
+
 	}
-	
+
 	/*
 	 * 最大公约数，欧几里得算法
 	 */
-	public static int gdc(int p, int q){
+	public static int gdc(int p, int q) {
 		if (q == 0) {
 			return p;
-		}else {
+		} else {
 			int r = p % q;
 			return gdc(q, r);
 		}
-		
+
 	}
 }
